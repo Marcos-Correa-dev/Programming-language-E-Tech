1d18f592eee22e61fd395481c6bc8b62f9284f9c
marcos.correa <marcos.correa@fpf.br>
2025-11-24 18:44:45 -0400

    first commit: example varieble with python

---
+++ main.py
@@ -0,0 +1,25 @@
+#  Inteiro
+numero = 10
+print(numero, type(numero)) # saida: 10 <class 'int'>
+
+# Float
+pi = 3.14159
+print(pi, type(pi))  # saida : 3.159159
+
+# String
+nome = 'Joao'
+print(nome, type(nome)) #saida: Joao
+
+# Booleano
+verdadeiro = True
+print(verdadeiro, type(verdadeiro)) # saida: True
+
+
+
+# Input
+
+idade = int(input('Qual a sua idade?: '))
+print("você tem ", idade, " anos")
+
+
+print
---
+++ varieble.py
@@ -0,0 +1,19 @@
+inteiro = 10        # int (número inteiro)
+decimal = 10.5      # float (número decimal)
+complexo = 3 + 4j   # complex (número complexo)
+
+# Texto
+texto = "Olá, mundo!"  # str (string/texto)
+
+# Booleanos
+verdadeiro = True      # bool (booleano verdadeiro)
+falso = False          # bool (booleano falso)
+
+# Coleções
+lista = [1, 2, 3]               # list (lista mutável)
+tupla = (1, 2, 3)               # tuple (lista imutável)
+dicionario = {"nome": "Maria"}  # dict (dicionário)
+conjunto = {1, 2, 3}            # set (conjunto)
+
+# Nulo
+nulo = None  # NoneType (valor nulo)
