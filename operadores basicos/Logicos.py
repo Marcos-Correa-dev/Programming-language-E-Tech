2d06c94862aa4a2a671c0d0538bceefe0dd33daa
marcos.correa <marcos.correa@fpf.br>
2025-11-26 17:50:37 -0400

    add logic and activity

---
+++ operadores basicos/Logicos.py
@@ -0,0 +1,36 @@
+# O operador and retorna True (Verdadeiro)
+# somente se ambas as condições comparadas
+# forem True.
+
+idade = 20
+possui_cnh = True
+
+# A pessoa é maior de idade E possui CNH?
+if idade >= 18 and possui_cnh == True:
+    print("Apto a dirigir legalmente.")
+else:
+    print("Não está apto.")
+# Output neste caso: Apto a dirigir legalmente.
+
+
+# O operador or retorna True se pelo menos
+# uma das condições comparadas for True.
+# Só retorna False se ambas forem False.
+# Sintaxe: condição1 or condição2
+# #
+# # Regra: False se False ou False, senão é True
+
+
+# dia = "Sábado"
+# feriado = False
+#
+# # É final de semana OU é feriado?
+# if dia == "Sábado" or dia == "Domingo" or feriado == True:
+#     print("É dia de folga!")
+# else:
+#     print("Dia útil.")
+# # Output neste caso: É dia de folga!
+
+
+
+
---
+++ operadores basicos/Não logicos.py
@@ -0,0 +1,11 @@
+# not (NÃO Lógico)
+# O operador not inverte o valor Booleano da condição.
+# Sintaxe: not condição
+
+
+usuario_logado = False
+
+# SE NÃO ESTÁ logado, peça para fazer login.
+if not usuario_logado:
+    print("Por favor, faça login.")
+# Output neste caso: Por favor, faça login.
